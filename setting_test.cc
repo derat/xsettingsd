bf7354553e21844ac29d5aaf6dc34106c859f1b1
Daniel Erat <dan@erat.org>
2009-08-08 15:41:39 -0700

    add test for writing colors

--- setting_test.cc
+++ setting_test.cc
@@ -26,10 +26,12 @@ testing::AssertionResult BytesAreEqual(
       string expected_str, actual_str, hl_str;
       bool first = true;
       for (size_t j = 0; j < size; ++j) {
-        expected_str += StringPrintf(" %02x", expected[j]);
-        actual_str += StringPrintf(" %02x", actual[j]);
+        expected_str +=
+            StringPrintf(" %02x", static_cast<unsigned char>(expected[j]));
+        actual_str +=
+            StringPrintf(" %02x", static_cast<unsigned char>(actual[j]));
         hl_str += (expected[j] == actual[j]) ? "   " : " ^^";
-        if ((j > 0 && (j % 16) == 0) || j == size - 1) {
+        if ((j % 16) == 15 || j == size - 1) {
           msg << (first ? "Expected:" : "\n         ") << expected_str << "\n"
               << (first ? "  Actual:" : "         ") << actual_str << "\n"
               << "         " << hl_str;
@@ -51,7 +53,7 @@ TEST(IntegerSettingTest, Write) {
   IntegerSetting setting(5);
   setting.UpdateSerial(NULL, 3);
   ASSERT_TRUE(setting.Write("name", &writer));
-  // TODO: Gotta fix a bunch of these for big-endian systems.
+  // TODO: Won't work on big-endian systems.
   const char expected[] = {
     0x0,                     // type
     0x0,                     // unused
@@ -72,7 +74,7 @@ TEST(StringSettingTest, Write) {
   StringSetting setting("testing");
   setting.UpdateSerial(NULL, 5);
   ASSERT_TRUE(setting.Write("setting", &writer));
-  // TODO: Gotta fix a bunch of these for big-endian systems.
+  // TODO: Won't work on big-endian systems.
   const char expected[] = {
     0x1,                     // type
     0x0,                     // unused
@@ -83,6 +85,30 @@ TEST(StringSettingTest, Write) {
     0x7, 0x0, 0x0, 0x0,      // value-len
     0x74, 0x65, 0x73, 0x74, 0x69, 0x6e, 0x67,  // "testing" (value)
     0x0,                     // padding
+  };
+  ASSERT_EQ(sizeof(expected), writer.bytes_written());
+  EXPECT_PRED_FORMAT3(BytesAreEqual, expected, buffer, sizeof(expected));
+}
+
+TEST(ColorSettingTest, Write) {
+  static const int kBufSize = 1024;
+  char buffer[kBufSize];
+
+  DataWriter writer(buffer, kBufSize);
+  ColorSetting setting(32768, 65535, 0, 255);
+  setting.UpdateSerial(NULL, 2);
+  ASSERT_TRUE(setting.Write("name", &writer));
+  // TODO: Won't work on big-endian systems.
+  const char expected[] = {
+    0x2,                     // type
+    0x0,                     // unused
+    0x4, 0x0,                // name-len
+    0x6e, 0x61, 0x6d, 0x65,  // "name" (multiple of 4, so no padding)
+    0x2, 0x0, 0x0, 0x0,      // serial
+    0x0, 0x80,               // red
+    0x0, 0x0,                // blue (yes, the order is strange)
+    0xff, 0xff,              // green
+    0xff, 0x0,               // alpha
   };
   ASSERT_EQ(sizeof(expected), writer.bytes_written());
   EXPECT_PRED_FORMAT3(BytesAreEqual, expected, buffer, sizeof(expected));
